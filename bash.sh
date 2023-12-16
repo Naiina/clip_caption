fedbf77550152e2bfc4a38bc3a622cdea1312e83
Coleman Haley <s2189251@login03.ecdf.ed.ac.uk>
2023-12-16 12:21:54 +0000

    merge bash.sh. we should probably re-organize the scheduler scripts so we don't have constant merge conflicts.

--- bash.sh
+++ bash.sh
@@ -6,7 +6,8 @@
 #$ -q gpu
 #$ -pe gpu-a100 1
 source ~/.bashrc
-conda activate /exports/eddie/scratch/s2523033/anaconda/envs/clip_prefix_caption/
+module load anaconda/2022.05
+conda activate clipcap
 #export DEEPLAKE_DOWNLOAD_PATH=/exports/eddie/scratch/s2523033
 
 data_folder="wit_big" 
@@ -23,9 +24,8 @@ python load_wit_dataset.py --outpath $data_folder --lang $lang --train_size $tra
 python parse_coco.py --dataset $data_folder --train_or_val "val"
 python parse_coco.py --dataset $data_folder --train_or_val "train"
 accelerate launch train.py --data "./data/${data_folder}/oscar_split_train.pkl" --out_dir $train_folder --epochs $epoch --model_name $model --prefix_length 10 --prefix_length_clip 10 
-#python train.py --data "./data/${data_folder}/oscar_split_train.pkl" --out_dir $train_folder --epochs $epoch --model_name $model--prefix_length 10 --prefix_length_clip 10 
 python predict.py --train_or_val "val" --train_path $train_folder --checkpoint $checkpoint --model_name $model --dataset $data_folder
-#python compute_score.py --path $train_folder --checkpoint $checkpoint --dataset_name $datafolder --metric $metric
+python compute_score.py --path $train_folder --checkpoint $checkpoint --dataset_name $data_folder --metric $metric
 conda deactivate
 
 
